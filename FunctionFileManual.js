0e9f7ea4b16f4ecc5ddef6dfbcd8c52404b51169
deputy382 <109768406+deputy382@users.noreply.github.com>
2025-11-29 10:28:20 -0500

    Add files via upload

--- FunctionFileManual.js
+++ FunctionFileManual.js
@@ -1,6 +1,6 @@
 function insertSignatureManual(event) {
     var signatureHtml = "<p style='font-family:Arial; font-size:12pt;'>Regards,<br><strong>FirstEnergy</strong><br>www.firstenergycorp.com</p>";
-    Office.context.mailbox.item.body.setSignatureAsync(signatureHtml, { coercionType: "html" }, function(result) {
+    Office.context.mailbox.item.body.setSelectedDataAsync(signatureHtml, { coercionType: "html" }, function(result) {
         if (result.status === Office.AsyncResultStatus.Succeeded) {
             console.log("Signature inserted successfully.");
         } else {
